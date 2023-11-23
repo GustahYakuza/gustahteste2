d4b0a79897b25c95b63bf5f055ec295440cd261b
GustahYakuza <129031822+GustahYakuza@users.noreply.github.com>
2023-11-23 03:07:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const glide = new Glide('.glide', {
-        type: 'carousel',
-        autoplay: 3000, // Configuração para auto avanço a cada 3 segundos
-    });
 
-    glide.mount();
-});
